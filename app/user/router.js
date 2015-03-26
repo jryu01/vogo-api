6f680b7bf37d5f17c90e78f4ce642c3e3c231680
Jaehwan Ryu <time0121@gmail.com>
2015-03-26 01:36:38 +0000

    change error handling

--- app/user/router.js
+++ app/user/router.js
@@ -19,77 +19,89 @@ var getUser = function (req, res, next) {
   User.findByIdAsync(req.params.id).then(res.json.bind(res)).catch(next);
 };
 
+var signinWithPassword = function (req, res, next) {
+  if (!req.body.email || !req.body.password) {
+    return res.status(401)
+              .json({ status: 401, message: 'Invalid credentials'});
+  }
+  User.findOneAsync({ email: req.body.email }).then(function (user) {
+    if (!user) {
+      res.status(401)
+        .json({status: 401, message: 'Can\'t find a user with that email'});
+    }
+    var token = jwt.encode({
+      iss: user.id,
+      exp: Date.now() + config.jwtexp
+    }, config.jwtsecret);
+    return user.comparePassword(req.body.password).then(function (match) {
+      if (!match) {
+        return res.status(401)
+                  .json({ status: 401, message: 'Password is not correct'});
+      }
+      res.json({
+        user: user,
+        access_token: token
+      });
+    });
+  }).catch(next);
+};
+
+var signinWithFacebook = function (req, res, next) {
+  if (!req.body.facebookAccessToken) {
+    return res.status(400)
+      .json({ status: 400, message: 'A facebook access token is required'});
+  }
+  request('https://graph.facebook.com/me?' +
+    'field=id,email,name&access_token=' + 
+    req.body.facebookAccessToken)
+  .spread(function (response, body) {
+    var profile;
+    if (response.statusCode !== 200) {
+      throw { 
+        name: 'FacebookGraphAPIError',
+        message: "Failed to fetch facebook user profile",
+        status: 500
+      };
+    }
+    return JSON.parse(body);
+  }).then(function (fbProfile) {
+    return User.findOneAsync({ 'facebook.id': fbProfile.id }).then(function (user) {
+      if (!user) {
+        var newUser = {
+          email: fbProfile.email,
+          name: fbProfile.name,
+          facebook: { 
+            id: fbProfile.id,
+            email: fbProfile.email,
+            name: fbProfile.name 
+          }
+        };
+        return User.createAsync(newUser);
+      }
+      return user;
+    });
+  }).then(function (user) {
+    var token = jwt.encode({
+      iss: user.id,
+      exp: Date.now() + config.jwtexp
+    }, config.jwtsecret);
+    res.json({ 
+      user: user,
+      access_token: token 
+    }); 
+  }).catch(function (e) {
+    if (e.name === 'FacebookGraphAPIError') {
+      return res.status(500).json(e);  
+    }
+    next(e); 
+  });
+};
+
 var signin = function (req, res, next) {
   if (req.body.grantType === 'password') {
-    if (!req.body.email || !req.body.password) {
-      return res.status(401)
-                .json({ status: 401, message: 'Invalid credentials'});
-    }
-    User.findOneAsync({ email: req.body.email }).then(function (user) {
-      if (!user) {
-        res.status(401)
-          .json({status: 401, message: 'Can\'t find a user with that email'});
-      }
-      var token = jwt.encode({
-        iss: user.id,
-        exp: Date.now() + config.jwtexp
-      }, config.jwtsecret);
-      return user.comparePassword(req.body.password).then(function (match) {
-        if (!match) {
-          return res.status(401)
-                    .json({ status: 401, message: 'Password is not correct'});
-        }
-        res.json({
-          user: user,
-          access_token: token
-        });
-      });
-    }).catch(next);
+    signinWithPassword(req, res, next);
   } else if (req.body.grantType === 'facebook') {
-    if (!req.body.facebookAccessToken) {
-      return res.status(400)
-        .json({ status: 400, message: 'A facebook access token is required'});
-    }
-    request('https://graph.facebook.com/me?' +
-      'field=id,email,name&access_token=' + 
-      req.body.facebookAccessToken)
-    .spread(function (response, body) {
-      var profile;
-      if (response.statusCode !== 200) {
-        return res.status(500).json({
-          name: 'FacebookGraphAPIError',
-          message: "Failed to fetch facebook user profile",
-          status: 500
-        });
-      }
-      return JSON.parse(body);
-    }).then(function (fbProfile) {
-      return User.findOneAsync({ 'facebook.id': fbProfile.id }).then(function (user) {
-        if (!user) {
-          var newUser = {
-            email: fbProfile.email,
-            name: fbProfile.name,
-            facebook: { 
-              id: fbProfile.id,
-              email: fbProfile.email,
-              name: fbProfile.name 
-            }
-          };
-          return User.createAsync(newUser);
-        }
-        return user;
-      });
-    }).then(function (user) {
-      var token = jwt.encode({
-        iss: user.id,
-        exp: Date.now() + config.jwtexp
-      }, config.jwtsecret);
-      res.json({ 
-        user: user,
-        access_token: token 
-      }); 
-    }).catch(next);
-      
+    signinWithFacebook(req, res, next);
   } else {
     return res.status(400).json({
       status: 400,
@@ -98,19 +110,6 @@ var signin = function (req, res, next) {
   }
 };
  
-
-// return User.findOneAsync({ email: email }).then(function (user) {
-//     if (!user) {
-//       throw { status: 401, message: 'Can\'t find a user with that email' };
-//     }
-//     return user.comparePassword( password ).then(function (match) {
-//       if (!match) {
-//         throw { status: 401, message: 'Password is not correct'};
-//       }
-//       return user;
-//     });
-//   });
-  
 router.post('/v2/users/signin', signin);
 
 router.post('/v2/users', createUser);
