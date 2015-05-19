140c92edb0ea7948b128ba06fa1dd892f919246b
Jaehwan Ryu <time0121@gmail.com>
2015-05-18 23:21:35 -0400

    cleanup

--- app/poll/poll.js
+++ app/poll/poll.js
@@ -12,23 +12,7 @@ var PollSchema = new Schema({
     name: { type: String, required: '{PATH} is required!' },
     picture: { type: String }
   },
-  createdAt: { type: Date, default: Date.now }, //will be depreciated
-  // updatedAt change when new vote or new comments are added 
-  updatedAt: { type: Date, default: Date.now },
 
-  // will be removed
-  subject1: {
-    text: { type: String },
-    numVotes: { type: Number, default: 0 }
-  },
-  subject2: {
-    text: { type: String },
-    numVotes: { type: Number, default: 0 }
-  },
-  subjectTexts: [],
-  /////////////////////
-
-  //// added /////
   question: { type: String, default: '' },
   answer1: {
     text: { type: String, default: '' },
@@ -42,17 +26,6 @@ var PollSchema = new Schema({
     picture: { type: String, default: '' },
     voters: [ Schema.Types.ObjectId ],
   },
-  //////////
-
-  totalNumVotes: { type: Number, default: 0 }, // be removed (total: a1 + a2)
-  votes: [{
-    voterId: { type: Schema.Types.ObjectId },
-    createdAt: { type: Date, default: Date.now }, //will be removed
-    subjectId: { type: Number }, //will be removed
-    subjectText: { type: String }, //will be removed
-    answer: { type: Number },
-    answerText : { type: String }
-  }],
 
   numComments: { type: Number, default: 0 },
   comments: [{
@@ -64,15 +37,9 @@ var PollSchema = new Schema({
     text: { type: String, default: '' }
   }],
 
-  _random: { type: Number, default: Math.random }
 });
 
 // Indexes
-PollSchema.index({'votes.voterId': 1, '_random': 1});
-PollSchema.index({'votes.voterId': 1, '_id': -1, '_random': 1});
-PollSchema.index({'votes.voterId': 1, 'votes.createdAt': -1 });
-PollSchema.index({'createdBy.userId': 1, 'updatedAt': -1 });
-
 PollSchema.index({'createdBy.userId': 1, '_id': -1 });
 
 //Add toJSON option to transform document before returnig the result
@@ -221,152 +188,4 @@ PollSchema.statics.getRecentUnvoted = function (user, beforePollId, exclude) {
   ]);
 };
 
-//VVVVVVVVVVVVVVVVVVVVVVVV Will be depreciated VVVVVVVVVVVVVVVVVVVVV
-PollSchema.methods.addVote = function (voterId, subjectId) {
-  var that = this;
-  if (!(subjectId === 1 || subjectId === 2)) {
-    return Promise
-      .reject(new Error('argument subjectId must be an integer 1 or 2'));
-  }
-  this.totalNumVotes += 1;
-  this['subject' + subjectId].numVotes += 1;
-  this.votes.push({
-    voterId: voterId,
-    subjectId: subjectId,
-    subjectText: this['subject' + subjectId].text 
-  });
-  return this.saveAsync().then(function (result) {
-    return result[0];
-  });
-};
-
-PollSchema.statics.createNew = function (data) {
-  var text1 = data.subject1 && data.subject1.text,
-      text2 = data.subject1 && data.subject2.text;
-  return this.createAsync({
-    subject1: {
-      text: text1
-    },
-    subject2: {
-      text: text2
-    },
-    subjectTexts: [text1, text2],
-    createdBy: {
-      name: data.user && data.user.name,
-      userId: data.user && data.user.id
-    }
-  });
-};
-// TODO: will be depreciated
-PollSchema.statics.findOneRandom = function (query, field, options) {
-  var that = this,
-  random = Math.random();
-
-  query = query || {};
-  query._random = { $gte: random };
-
-  return that.findOneAsync(query, field, options).then(function (poll) {
-    if (!poll) {
-      query = _.clone(query); // this only needed to pass unit test using sinon
-      query._random = { $lte: random };
-      return that.findOneAsync(query, field, options);
-    }
-    return poll;
-  });
-};
-// TODO: will be depreciated
-PollSchema.statics.findOneRandomNew = function (query, field, options) {
-  var that = this,
-  random = Math.random();
-
-  query = query || {};
-  query._random = { $gte: random };
-
-  options = options || {};
-  options.sort = { _random: 1 };
-
-  return that.findOneAsync(query, field, options).then(function (poll) {
-    if (!poll) {
-      query = _.clone(query); // this only needed to pass unit test using sinon
-      delete query._random;
-      return that.findOneAsync(query, field, options).then(function (poll) {
-        if (!poll) {
-          return null;
-        }
-        poll._random = Math.random();
-        return poll.saveAsync().then(function (result) {
-          return result[0];
-        });
-      });
-    }
-    poll._random = Math.random();
-    return poll.saveAsync().then(function (result) {
-      return result[0];
-    });
-  });
-};
-
-////////////////////////////////////////////////
-// Instance Methods
-PollSchema.methods.addVote = function (voterId, subjectId) {
-  var that = this;
-  if (!(subjectId === 1 || subjectId === 2)) {
-    return Promise
-      .reject(new Error('argument subjectId must be an integer 1 or 2'));
-  }
-  this.totalNumVotes += 1;
-  this['subject' + subjectId].numVotes += 1;
-  this.votes.push({
-    voterId: voterId,
-    subjectId: subjectId,
-    subjectText: this['subject' + subjectId].text 
-  });
-  return this.saveAsync().then(function (result) {
-    return result[0];
-  });
-};
-// var getRandom = function (req, res) {
-//   var query = { 'votes.voterId': { $ne: req.user.id } },
-//       exclude = req.query.exclude;
-
-//   // exclude polls with provided ids
-//   if (exclude && exclude !== 'undefined') {
-//     // concat becaulse exlucde can be either single value or array
-//     query._id = { $nin: [].concat(exclude) };
-//   }
-//   return Poll.findOneRandomNew(query);
-// };
-//TODO: need test
-PollSchema.statics.getRecommendations = function (query, field) {
-  var that = this,
-      result = [],
-      options = {},
-      random = Math.random();
-
-  query = query || {};
-  query._random = { $gte: random };
-
-  options.sort = { _random: 1 };
-  options.limit = 10;
-
-  return that.findAsync(query, field, options).then(function (poll) {
-    var resultPromise;
-    result = result.concat(poll);
-    resultPromise = Promise.resolve(result);
-
-    if (result.length < 10) {
-      query._random = { $lt: random };
-      options.limit = 10 - result.length;
-
-      resultPromise = that.findAsync(query, field, options)
-      .then(function (poll) {
-        result = result.concat(poll);
-        return result;
-      });
-    }
-    return resultPromise;
-  });
-};
-
-//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 module.exports = mongoose.model('Poll', PollSchema);
--- app/poll/pollRouter.js
+++ app/poll/pollRouter.js
@@ -6,121 +6,7 @@ var requireToken = require('app/middleware/requireToken'),
     Poll = require('./poll'),
     Vote = require('./vote'),
     _ = require('lodash');
-    
-var list = function (req, res, next) {
 
-  if (req.query.random === "true" ) {
-    return getRecommendations(req, res, next);
-  }
-
-  var query = {}, 
-      options = {
-        sort: { updatedAt: -1 },
-        limit: 10
-      },
-      projection = {
-        'createdBy': 1,
-        'createdAt': 1,
-        'updatedAt': 1,
-        'subject1': 1,
-        'subject2': 1,
-        'subjectTexts': 1,
-        'totalNumVotes': 1,
-        'tags': 1
-      };
-
-  if (req.query.before) {
-    query.updatedAt = { $lt: req.query.before };
-  }
-
-  if (req.query.voterId) {
-    query['votes.voterId'] = mongoose.Types.ObjectId(req.query.voterId);
-    // projection['votes.$'] = 1;
-    if (req.query.votedBefore) {
-      query['votes.createdAt'] = { $lt: new Date(req.query.votedBefore) };
-    }
-    return Poll.aggregateAsync([
-      { $match: { "votes.voterId": query['votes.voterId'] } },
-      { $unwind: "$votes" },
-      { $match:  query },
-      { $sort: { "votes.createdAt": -1} },
-      { $limit: 10 } 
-    ]).then(function (polls) {
-      polls.forEach(function (poll) {
-        poll.votes = [poll.votes];
-      });
-      return polls;
-    });
-  }
-  return Poll.findAsync(query, projection, options);
-};
-
-var getRecommendations = function (req, res) {
-  var query = { 'votes.voterId': { $ne: req.user.id } },
-      exclude = req.query.exclude;
-
-  // exclude polls with provided ids
-  if (exclude && exclude !== 'undefined') {
-    // concat because exlucde can be either single value or array
-    query._id = { $nin: [].concat(exclude) };
-  }
-  return Poll.getRecommendations(query);
-};
-
-var listMyPoll = function (req, res) {
-  var user = req.user;
-  var query = { 'createdBy.userId': user.id };
-
-  if (req.query.before) {
-    query.updatedAt = { $lt: req.query.before };
-  }
-  var options = {
-    sort: { updatedAt: -1 },
-    limit: 10
-  };
-  return Poll.findAsync(query, null, options);
-};
-
-var create = function (req, res, next) {
-  _.extend(req.body, { user: req.user });
-  Poll.createNew(req.body).then(res.status(201).json.bind(res)).catch(next);
-};
-
-var getRandom = function (req, res) {
-  var query = { 'votes.voterId': { $ne: req.user.id } },
-      exclude = req.query.exclude;
-
-  // exclude polls with provided ids
-  if (exclude && exclude !== 'undefined') {
-    // concat becaulse exlucde can be either single value or array
-    query._id = { $nin: [].concat(exclude) };
-  }
-  return Poll.findOneRandomNew(query);
-};
-
-var createVote = function (req, res) {
-  var subjectId = req.body.subjectId,
-      voterId = req.user.id,
-      query = {
-        _id: req.params.id,
-        'votes.voterId': { $ne: voterId }
-      };
-  return Poll.findOneAsync(query).then(function (poll) {
-    if (!poll) {
-      throw { status: 400, message: 'Could not create new vote'};
-    }
-    return poll.addVote(voterId, subjectId);
-  });
-};
-
-function res(promiseReturningFunction) {
-  return function (req, res, next) {
-    promiseReturningFunction(req, res, next).then(function (result) {
-      res.json(result);
-    }).catch(next);
-  };
-}
-//////////////////////////////////////////////////////////////////
 var publish = function (req, res, next) {
   //TODO: input validation with req.body
   Poll.publish(req.user, req.body)
@@ -198,17 +84,6 @@ var pollRouter = module.exports = function () {
   
   var router = express.Router();
   
-  // router.all('/me*', requireToken);
-  // router.get('/me/polls', res(listMyPoll));
-  
-  // router.all('/polls*', requireToken);
-  // router.post('/polls', create); 
-  // router.get('/polls', res(list));
-  // router.get('/polls/random', res(getRandom)); 
-  // router.post('/polls/:id/votes', res(createVote));
-  // router.get('/polls/recommendation', requireToken, getRecommendations);
-
-/////////////////////////////////////////////
   router.post('/polls', requireToken, publish);
   router.post('/polls/:id/votes', requireToken, vote);
   router.post('/polls/:id/comments', requireToken, comment);
